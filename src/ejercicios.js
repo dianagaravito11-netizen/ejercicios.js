9e4916e58cf2cfcb1ab68e2c2dc274bf21ba8526
Diana Garavito <dianagaravito11@gmail.com>
2026-03-05 21:21:26 -0500

    ejercicio 28,29

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -177,14 +177,14 @@ function combinarNombres(nombre, apellido) {return nombre + " " + apellido
 }
 
 // Test 28
-function obtenerSaludo(nombre) {
+function obtenerSaludo(nombre) {return "Hola " + nombre + "!"
 	// Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
 	// "Martin" -> "Hola Martin!"
 	// Tu código:
 }
 
 // Test 29
-function obtenerAreaRectangulo(alto, ancho) {
+function obtenerAreaRectangulo(alto, ancho) {return alto * ancho
 	// Retornar el area de un rectángulo teniendo su altura y ancho
 	// Tu código:
 }
