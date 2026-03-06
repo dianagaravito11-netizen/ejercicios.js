54992b1ebd382ba011bc1ef60dc9fc461b470ef0
Diana Garavito <dianagaravito11@gmail.com>
2026-03-05 21:48:00 -0500

    ejercicio 32, 33,34,35

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -202,14 +202,14 @@ function areaDelTriangulo(base, altura) { return (base * altura) / 2
 }
 
 // Test 32
-function deEuroAdolar(euro) {
+function deEuroAdolar(euro) {return euro*1.2
 	//Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
 	//como parámetro un número de euros y calcule el cambio en dólares.
 	//Escribe tu código aquí
 }
 
 // Test 33
-function esVocal(letra) {
+function esVocal(letra) {if (letra.legth > 1) {return "Dato incorrecto";}
 	//Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
 	//Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle
 	//que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
